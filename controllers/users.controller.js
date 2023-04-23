c3b6edc60f6bd8db81b98091ce97aaa55f0f5047
ViolettaKozak <112846006+ViolettaKozak@users.noreply.github.com>
2023-04-23 15:22:39 +0300

    finish

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,7 @@
 const { head } = require("request");
 const axios = require('axios');
 const response = require("response");
+const { json } = require("express");
 
 const users = [
     {
@@ -23,31 +24,66 @@ const users = [
     },
 ];
 
-const getUsers = async (req, res) => {
-    try {
-        const response = await axios.get(`https://gorest.co.in/public/v2/users` , {
+async function conect(method, data, params){
+    if(method === 'get'){
+        if(params){
+            const response = axios.get(`https://gorest.co.in/public/v2/users/${params}` , {
+                headers: {
+                    Authorization: `Bearer b2e94a33167c9bfed20be08e992d8f69958f882bc40c811773571cdd379a2c5a`
+                }
+            });
+            return response;
+        }
+        else{
+            const response = axios.get(`https://gorest.co.in/public/v2/users` , {
+                headers: {
+                    Authorization: `Bearer b2e94a33167c9bfed20be08e992d8f69958f882bc40c811773571cdd379a2c5a`
+                }
+            });
+            return response;
+        }
+    }
+    else if(method === 'post'){
+        const response = axios.post('https://gorest.co.in/public/v2/users', data, {
             headers: {
                 Authorization: `Bearer b2e94a33167c9bfed20be08e992d8f69958f882bc40c811773571cdd379a2c5a`
             }
         });
-        const user = response.data;
-        res.status(200).json(user);
-      } catch (error) {
-        res.status(500).json({ message: 'Error fetching users' });
-      }
+        return response;
+    }
+    else if(method === 'put'){
+        const response = axios.put(`https://gorest.co.in/public/v2/users/${params}`,data ,{
+            headers: {
+                Authorization: `Bearer b2e94a33167c9bfed20be08e992d8f69958f882bc40c811773571cdd379a2c5a`
+            }
+        });
+        return response;
+    }
+    else if (method === 'del'){
+        const response = axios.delete(`https://gorest.co.in/public/v2/users/${params}`, {
+            headers: {
+                Authorization: `Bearer b2e94a33167c9bfed20be08e992d8f69958f882bc40c811773571cdd379a2c5a`
+            }
+        });
+        return response;
+    }
+}
+
+const getUsers = async (req, res) => {
+    try{
+        result = await conect('get');
+        res.status(201).json(result.data);
+    }
+    catch(error){
+        res.status(500).json({message: "Error fetching users"});
+    }
 };
 
 const getUser = async (req, res) => {
     const {id} = req.params;
-    const url = 'https://gorest.co.in/public/v2/users/' + id;
     try{
-        const response = await axios.get(url, {
-            headers: {
-                Authorization: `Bearer b2e94a33167c9bfed20be08e992d8f69958f882bc40c811773571cdd379a2c5a`
-            }
-        });
-        const user = response.data;
-        res.status(200).json(user);
+        const response = await conect('get', 0, id);
+        res.status(201).json(response.data);
     }catch(error){
         res.status(500).json({message: "Error fetching user"})
     }
@@ -56,12 +92,8 @@ const getUser = async (req, res) => {
 const addUser = async (req, res) => {
     const user = req.body;
     try{
-        const response = await axios.post('https://gorest.co.in/public/v2/users', user, {
-            headers: {
-                Authorization: `Bearer b2e94a33167c9bfed20be08e992d8f69958f882bc40c811773571cdd379a2c5a`
-            }
-        });
-        res.status(201).json(response.data.data);
+        const response = await conect('post', user);
+        res.status(201).json(response.data);
     }catch(error) {
         res.status(500).json({message: 'Error adding user'});
     }
@@ -70,18 +102,8 @@ const addUser = async (req, res) => {
 const updateUser = async (req, res) =>{
     const {name, email, gender, status} = req.body;
     const { id } = req.params;
-    const url = 'https://gorest.co.in/public/v2/users/' + id;
     try{
-        const response = await axios.put(url ,{
-            name,
-            email,
-            gender,
-            status
-        } ,{
-            headers: {
-                Authorization: `Bearer b2e94a33167c9bfed20be08e992d8f69958f882bc40c811773571cdd379a2c5a`
-            }
-        });
+        const response = await conect('put', {name, email, gender, status}, id);
         res.status(200).json(response.data);
     }catch(error){
         res.status(500).json({message: "Error updating user"})
@@ -90,14 +112,9 @@ const updateUser = async (req, res) =>{
 
 const delUser = async (req, res) => {
     const {id} = req.params;
-    const url = 'https://gorest.co.in/public/v2/users/' + id;
     try{
-        const response = await axios.delete(url, {
-            headers: {
-                Authorization: `Bearer b2e94a33167c9bfed20be08e992d8f69958f882bc40c811773571cdd379a2c5a`
-            }
-        });
-        res.status(203).end();
+        const response = await conect('del', 0, id );
+        res.status(200).end("User deleted");
     }catch(error){
         res.status(500).json({message: "Error deleting user"})
     }
